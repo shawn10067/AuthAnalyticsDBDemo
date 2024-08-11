cdf9ac1ce7fd052b64898de175d1a8ec34acde26
shawn10067 <shawn10067@gmail.com>
2024-08-11 00:30:59 -0400

    fix: refactored some code for nativewind

--- app/user/index.tsx
+++ app/user/index.tsx
@@ -1,7 +1,7 @@
 import {useAuthStore} from '@/store/authStore';
 import {useRouter} from 'expo-router';
 import {useState} from 'react';
-import {ActivityIndicator, Button, View, Text} from 'react-native';
+import {ActivityIndicator, Button, View, Text, Pressable} from 'react-native';
 
 const WelcomeScreen = () => {
   const [signingOut, setSigningOut] = useState(false);
@@ -10,26 +10,22 @@ const WelcomeScreen = () => {
   const logOut = useAuthStore(state => state.logOut);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
+    <View className="bg-slate-800 flex-1 justify-center items-center">
       <Text className="text-orange-300 text-2xl">Welcome, {user?.email}!</Text>
       {signingOut ? (
         <ActivityIndicator />
       ) : (
-        <Button
-          title="Log out"
+        <Pressable
+          className="h-12 items-center justify-center p-4 bg-slate-600 rounded-md active:bg-blue-400"
           onPress={async () => {
             setSigningOut(true);
             await logOut();
             router.navigate('/');
             setSigningOut(false);
           }}
-        />
+        >
+          <Text className="text-orange-300">Sign out</Text>
+        </Pressable>
       )}
       <Button title="Go to snap!" onPress={() => router.navigate('/snap')} />
     </View>
